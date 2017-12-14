ae2070ba55e001b948847b0619113b262669274d
Heather Palmer <heatherpa@zillow.com>
2017-12-14 13:55:22 -0800

    Add CSS grid for the category buttons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,18 +6,32 @@ class Nav extends Component {
     return (
         //change all of these buttons to DROPDOWN menus? Remember static-SG dropdwon (very complex) or make clickable and show options on the link that will appear (after clicked)
         <nav className="enlyten-navbar">
-            <div className="enlyten-dropdowns">
-                <button className="dropdown">
+{/*            <div>
+                <button className="button" id="buttonPortrait">
                   <a href="#">PORTRAIT PHOTOGRAPHY</a>
                 </button>
-                <button className="dropdown">
+                <button className="button" id="buttonArtistic">
                   <a href="#">ARTISTIC PHOTOGRAPHY</a>
                 </button>
-                <button className="dropdown">
+                <button className="button" id="buttonEvent">
                   <a href="#">EVENT PHOTOGRAPHY</a>
                 </button>
-                <button className="dropdown">
+                <button className="button" id="buttonThePhotographer">
                   <a href="#">THE PHOTOGRAPHER</a>
+                </button>
+            </div>*/}
+            <div class="wrapper">
+                <button class="one button-styles">
+                    <a href="#">PORTRAITS</a>
+                </button>
+                <button class="two button-styles">
+                    <a href="#">ARTISTIC</a>
+                </button>
+                <button class="three button-styles">
+                    <a href="#">EVENTS</a>
+                </button>
+                <button class="four button-styles">
+                    <a href="#">THE PHOTOGRAPHER</a>
                 </button>
             </div>
         </nav>
